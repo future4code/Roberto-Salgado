758269f39127f9b90e60a01deeea8527c8f67201
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2020-08-07 12:50:57 -0300

    Adicionando os arquivos para iniciar os exercícios e os exercícios de interpretação feitos.

---
+++ semana-4/lista-de-exercios/index.js
@@ -0,0 +1,36 @@
+// ### --- Lista de Exercícios: --- ###
+
+// ------------------------ Exercícios de interpretação de código
+
+// --- 1.
+// Cria uma função que pergunta o valor da cotação atual do dólar, retorna o 
+// valor de dolar solicitado no argumento da função multiplicado pela cotação 
+// e antecedido da notação de Real, executa a função para 100 dólares, 
+// armazenando em uma nova constante e depois imprime este valor no console.
+
+// --- 2.
+// Cria uma função que recebe um tipo de investimento e um valor. Então roda 
+// uma condicional para cada tipo de investimento, alertanto o usuário para
+// tipos inválidos, e, para cada caso, multiplica o valor do argumento pelo
+// valor referente a cada tipo. Então, retorno o valor após a operação. Depois
+// gera duas novas constantes, chamando a função para testar valores 
+// diferentes para tipos de investimento diferentes e imprime estes dois 
+// valores na tela. 
+
+// --- 3.
+
+
+
+// ------------------------ Exercícios de Lógica de Programação
+
+// ---
+
+// ------------------------- Exercícios de Funções
+
+// ---
+
+// ------------------------- Exercícios de Objetos
+
+// ---
+
+// ------------------------- Exercícios de Funções de array
