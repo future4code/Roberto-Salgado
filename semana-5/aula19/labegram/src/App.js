a0e3613abd5d312f81fe96ae2bcef7dffa1e6cdc
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2020-08-13 14:53:20 -0300

    Adicionando a 1ª implementação da Semana5-Aula20.

--- semana-5/aula19/labegram/src/App.js
+++ semana-5/aula19/labegram/src/App.js
@@ -3,25 +3,42 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+
+  state = {
+    posts: [
+      {
+        nomeUsuario: 'paulinha',
+        fotoUsuario: 'https://picsum.photos/50/50?a=1',
+        fotoPost: 'https://picsum.photos/200/150?a=1'
+      },
+      {
+        nomeUsuario: 'darvasso',
+        fotoUsuario: 'https://picsum.photos/50/50?a=2',
+        fotoPost: 'https://picsum.photos/200/150?a=2'
+      },
+      {
+        nomeUsuario: 'dabreu',
+        fotoUsuario: 'https://picsum.photos/50/50?a=3',
+        fotoPost: 'https://picsum.photos/200/150?a=3'
+      }
+    ]
+  }
+
+
   render() {
+
+    const listaPosts = this.state.posts.map((post) => {
+      return (
+        <Post 
+          nomeUsuario = { post.nomeUsuario }
+          fotoUsuario = { post.fotoUsuario }
+          fotoPost = { post.fotoPost }
+        />
+      )
+    })
+
     return (
-      <div className={'app-container'}>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50?a=1'}
-          fotoPost={'https://picsum.photos/200/150?a=1'}
-        />
-        <Post
-          nomeUsuario={'darvasso'}
-          fotoUsuario={'https://picsum.photos/50/50?a=2'}
-          fotoPost={'https://picsum.photos/200/150?a=2'}
-        />
-        <Post
-          nomeUsuario={'dabreu'}
-          fotoUsuario={'https://picsum.photos/50/50?a=3'}
-          fotoPost={'https://picsum.photos/200/150?a=3'}
-        />
-      </div>
+      <div className={'app-container'}>{listaPosts}</div>
     );
   }
 }
