0dc74a6f1e1c85141c193a0e64cdee41bf64266f
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2020-08-27 19:30:18 -0300

    Finalizando estilização preliminar.

--- semana-7/aula27/apis_na_natureza/src/App.js
+++ semana-7/aula27/apis_na_natureza/src/App.js
@@ -1,8 +1,69 @@
 import React from 'react'
-import './styles.css'
-import axios from 'axios'
+import axios from 'axios' // eslint-disable-next-line
 import styled from 'styled-components'
 import { baseUrl } from './assets/constants/axiosConstants'
+
+const PokedexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const PokemonCard = styled.div`
+  min-width: 240px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: gray;
+  padding: 2em;
+  margin-top: 1.5em;
+  border-radius: 0.8rem;
+
+  img {
+    width: 200px;
+  }
+`
+const PokemonCardTitle = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 1em;
+
+  span {
+    font-size: 24px;
+    font-weight: 600;
+    background-color: white;
+    padding: 0.4em;
+    border-radius: 0.4em;
+  }
+`
+
+const DetailsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h3 {
+    margin: 0 0 1em 0;
+  }
+`
+
+const PokemonTypesContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 1em;
+  background-color: white;
+  border-radius: 1.6em;
+  padding: 0.8em 0;
+`
+
+const PokemonTypes = styled.span`
+  background-color: black;
+  color: white;
+  border-radius: 8px;
+  padding: 0.2em 0.5em;
+`
 
 export default class App extends React.Component {
   state = {
@@ -57,39 +118,42 @@ export default class App extends React.Component {
   render() {
 
     const pokemonCard = (
-      <div>
-        <h2>
-          { this.state.pokemonName } <span>#{ this.state.pokemonId }</span>
-        </h2>
+      <PokemonCard>
+        <PokemonCardTitle>
+          <span>{ this.state.pokemonName }</span>
+          <span>#{ this.state.pokemonId }</span>
+        </PokemonCardTitle>
         <img alt={'Pokemon Snap'} src={this.state.pokemonSnap} />
-        <div>
+        <DetailsContainer>
           <h3>Tipos:</h3>
+          <PokemonTypesContainer>
           { this.state.pokemonTypes.map(type => {
             return (
-              <span key={ type.slot }>
+              <PokemonTypes key={ type.slot }>
                 { this.capitalize(type.type.name) }
-              </span>
+              </PokemonTypes>
             )
           }) }
-        </div>
-      </div>
+          </PokemonTypesContainer>
+        </DetailsContainer>
+      </PokemonCard>
     )
 
     return (
-      <div className="App">
+      <PokedexContainer>
         <h1>Pokedex 1st Gen</h1>
         <select onChange={this.iChooseYou}>
           <option value={""}></option>
           {this.state.pokedex.map((poke) => {
             return (
               <option key={poke.name} value={poke.name}>
-                {poke.name}
+                {this.capitalize(poke.name)}
               </option>
             )
           })}
         </select>
         {this.state.pokemonId && pokemonCard}
-      </div>
+      </PokedexContainer>
     )
   }
 }
