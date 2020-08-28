7735574739c9b4aa8602694e0a622f2af5fcb948
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2020-08-27 23:26:11 -0300

    Adicionando componentes Lyrics e StarWars.

--- semana-7/aula27/apis_na_natureza/src/App.js
+++ semana-7/aula27/apis_na_natureza/src/App.js
@@ -1,159 +1,48 @@
-import React from 'react'
-import axios from 'axios' // eslint-disable-next-line
+import React from 'react' // eslint-disable-next-line
 import styled from 'styled-components'
-import { baseUrl } from './assets/constants/axiosConstants'
-
-const PokedexContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
-
-const PokemonCard = styled.div`
-  min-width: 240px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: gray;
-  padding: 2em;
-  margin-top: 1.5em;
-  border-radius: 0.8rem;
-
-  img {
-    width: 200px;
-  }
-`
-const PokemonCardTitle = styled.div`
-  display: flex;
-  justify-content: center;
-  gap: 1em;
-
-  span {
-    font-size: 24px;
-    font-weight: 600;
-    background-color: white;
-    padding: 0.4em;
-    border-radius: 0.4em;
-  }
-`
-
-const DetailsContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  h3 {
-    margin: 0 0 1em 0;
-  }
-`
-
-const PokemonTypesContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  gap: 1em;
-  background-color: white;
-  border-radius: 1.6em;
-  padding: 0.8em 0;
-`
-
-const PokemonTypes = styled.span`
-  background-color: black;
-  color: white;
-  border-radius: 8px;
-  padding: 0.2em 0.5em;
-`
+import { PokeAPI } from './assets/components/PokeAPI'
+import { Lyrics } from './assets/components/Lyrics'
+import { StarWars } from './assets/components/StarWars'
 
 export default class App extends React.Component {
   state = {
-    pokedex: [],
-    pokemonName: '',
-    pokemonId: '',
-    pokemonSnap: '',
-    pokemonTypes: []
+    currentPage: ''
   }
 
-  capitalize = (s) => {
-    if (typeof s !== 'string') return ''
-    return s.charAt(0).toUpperCase() + s.slice(1)
+  onChangePage = e => {
+    this.setState({currentPage: e.target.value})
   }
-
-  gottaCatchThemAll = () => {
-    axios
-      .get(`${baseUrl}/?limit=151`)
-      .then((response) => {
-        this.setState({ pokedex: response.data.results })
-      })
-      .catch((error) => {
-        console.log(error.message)
-      })
-  }
-
-  catchPokeData = (name) => {
-    axios
-      .get(`${baseUrl}/${name}`)
-      .then((response) => {
-        this.setState({ 
-          pokemonName: this.capitalize(response.data.name),
-          pokemonId: response.data.id,
-          pokemonSnap: response.data.sprites.front_default,
-          pokemonTypes: response.data.types
-        })
-        console.log(this.state.pokemonTypes)
-      })
-      .catch((error) => {
-        console.log(error.message)
-      })
-  }
-
-  iChooseYou = e => {
-    this.catchPokeData(e.target.value)
-  }
-
-  componentDidMount() {
-    this.gottaCatchThemAll()
-  }
-
+  
   render() {
 
-    const pokemonCard = (
-      <PokemonCard>
-        <PokemonCardTitle>
-          <span>{ this.state.pokemonName }</span>
-          <span>#{ this.state.pokemonId }</span>
-        </PokemonCardTitle>
-        <img alt={'Pokemon Snap'} src={this.state.pokemonSnap} />
-        <DetailsContainer>
-          <h3>Tipos:</h3>
-          <PokemonTypesContainer>
-          { this.state.pokemonTypes.map(type => {
-            return (
-              <PokemonTypes key={ type.slot }>
-                { this.capitalize(type.type.name) }
-              </PokemonTypes>
-            )
-          }) }
-          </PokemonTypesContainer>
-        </DetailsContainer>
-      </PokemonCard>
-    )
+    const currentPage = this.state.currentPage
+    let selectedPage = ''
+    
+    
+    switch(currentPage) {
+      case 'PokeAPI':
+        selectedPage = <PokeAPI />
+        break
+      case 'Lyrics':
+        selectedPage = <Lyrics />
+        break
+      case 'Star Wars':
+        selectedPage = <StarWars />
+        break
+      default:
+        break
+    }
 
     return (
-      <PokedexContainer>
-        <h1>Pokedex 1st Gen</h1>
-        <select onChange={this.iChooseYou}>
-          <option value={""}></option>
-          {this.state.pokedex.map((poke) => {
-            return (
-              <option key={poke.name} value={poke.name}>
-                {this.capitalize(poke.name)}
-              </option>
-            )
-          })}
+      <div>
+        <select onChange={this.onChangePage}>
+          <option value={''}></option>
+          <option value={'PokeAPI'}>PokeAPI</option>
+          <option value={'Lyrics'}>Lyrics</option>
+          <option value={'Star Wars'}>Star Wars</option>
         </select>
-        {this.state.pokemonId && pokemonCard}
-      </PokedexContainer>
+        { selectedPage }
+      </div>
     )
   }
 }
--- semana-7/aula27/apis_na_natureza/src/assets/constants/axiosConstants.js
+++ semana-7/aula27/apis_na_natureza/src/assets/constants/axiosConstants.js
@@ -1,2 +1,8 @@
-export const baseUrl = 
+export const pokeUrl = 
   'https://pokeapi.co/api/v2/pokemon'
+
+export const lyricsUrl = 
+  'https://api.lyrics.ovh/v1'
+
+export const swUrl = 
+  'https://swapi.dev/api/films'
