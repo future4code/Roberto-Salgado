bf53bb50bc26305220062e3bfdae846b99c53acf
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2020-09-20 20:01:59 -0300

    Isolando função request da tela ListTrips.

--- semana-10/projeto/labex/src/assets/screens/ListTripsPage/ListTripsPage.js
+++ semana-10/projeto/labex/src/assets/screens/ListTripsPage/ListTripsPage.js
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, {  useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-import axios from 'axios'
 import { 
   goToHomePage, 
   goToLoginPage,
   goToTripDetailsPage, 
-  goToCreateTripPage, 
-  goBack 
+  goToCreateTripPage,
 } from '../../actions/goToPages'
 import { baseUrl } from '../../constants/axiosConstants'
+import { useGetTrips } from '../../hooks/useRequestData'
 import {
   ListTripsScreenWrapper,
   TripsListWrapper,
@@ -17,23 +16,14 @@ import {
 } from './styled'
 
 const ListTripsPage = () => {
-  const [tripsList, setTripsList] = useState([])
+  const tripsList = useGetTrips(`${ baseUrl }/trips`, [])
   const history = useHistory()
   
   useEffect(() => {
     const token = window.localStorage.getItem("token")
 
-    token ? getTrips() : goToLoginPage(history)
+    !token && goToLoginPage(history)
   }, [history])
-
-  const getTrips = () => {
-    axios
-      .get(`${ baseUrl }/trips`)
-      .then(response => {
-        setTripsList(response.data.trips)
-      })
-      .catch(error => {})
-  }
 
   const handleTripClick = (tripId) => {
     goToTripDetailsPage(history, tripId)
