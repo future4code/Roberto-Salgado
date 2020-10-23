06cb8176dbffb08dc209436a4f28c8034e89456f
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2020-10-23 17:57:45 -0300

    Requisito mínimo 7

--- semana-15/projeto/labebank/src/handleData.ts
+++ semana-15/projeto/labebank/src/handleData.ts
@@ -1,11 +1,8 @@
-const currentDate: Date = new Date()
-
-const dateOfBirthAsString: string = "01/04/1970"
-
-const [day, month, year] = dateOfBirthAsString.split("/")
-
-const dateOfBirth: Date = new Date(`${year}-${month}-${day}`)
-
-const ageInMs: number = currentDate.getTime() - dateOfBirth.getTime()
-
-const ageInYears: number = ageInMs/1000/60/60/24/365
+export const getAge = (dateStr: string): number => {
+  const currentDate: Date = new Date()
+  const [day, month, year] = dateStr.split("/")
+  const date: Date = new Date(`${year}-${month}-${day}`)
+  const ageInMs: number = currentDate.getTime() - date.getTime()
+  const ageInYears: number = ageInMs/1000/60/60/24/365
+  return ageInYears
+}
--- semana-15/projeto/labebank/src/index.ts
+++ semana-15/projeto/labebank/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import { accounts, account, transaction, user } from "./accounts";
+import { getAge } from "./handleData";
 import { AddressInfo } from "net";
 
 const app: Express = express();
@@ -14,7 +15,7 @@ app.get("/users", (req: Request, res: Response): void =>{
       name: account.user.name,
       id: account.user.id,
       dateOfBirth: account.user.dateOfBirth
-    }))
+    }));
 
 		res.status(200).send(result);
 	}catch(error){
@@ -25,8 +26,8 @@ app.get("/users", (req: Request, res: Response): void =>{
 });
 
 app.post("/users", (req: Request, res: Response)=>{
-  let errorCode: number = 400
-  let errorMessage: string = "Error creating account"
+  let errorCode: number = 400;
+  let errorMessage: string = "Error creating account";
   type successMessage = {
     message: string,
     account: account
@@ -36,26 +37,33 @@ app.post("/users", (req: Request, res: Response)=>{
 		const {name, id, dateOfBirth} = req.body;
 
     if(!name || !id || !dateOfBirth){
-      errorCode = 400
-      errorMessage = "Favor informar nome, CPF e data de nascimento."
-      throw new Error()
+      errorCode = 400;
+      errorMessage = "Favor informar nome, CPF e data de nascimento.";
+      throw new Error();
     }
 
-    const userIndex: number = accounts.findIndex(account => account.user.id === id)
+    const userIndex: number = accounts.findIndex(account => account.user.id === id);
     if(userIndex > -1) {
-      errorCode = 403
-      errorMessage = "CPF já cadastrado"
-      throw new Error()
+      errorCode = 403;
+      errorMessage = "CPF já cadastrado";
+      throw new Error();
+    }
+    
+    const newUserAge: number = getAge(dateOfBirth);
+    if(newUserAge < 18){
+      errorCode = 403;
+      errorMessage = "Não é permitido abrir conta para menores de 18 anos";
+      throw new Error();
     }
 
-    const newAccount: account = {user: {name, id, dateOfBirth}, balance: 0, statement: []}
+    const newAccount: account = {user: {name, id, dateOfBirth}, balance: 0, statement: []};
 
-    accounts.push(newAccount)
+    accounts.push(newAccount);
 
     const successMessage: successMessage = {
       "message": "Success!",
       "account": newAccount
-    }
+    };
 
     res.status(200).send(successMessage)
   } catch(error) {
