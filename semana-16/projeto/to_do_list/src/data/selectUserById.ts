db6ffb165489570cc3fccbc82ec216a939bd3140
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2020-10-30 17:29:09 -0300

    Adicionando melhorias a funcionalidade Pegar usuário pelo id

--- semana-16/projeto/to_do_list/src/data/selectUserById.ts
+++ semana-16/projeto/to_do_list/src/data/selectUserById.ts
@@ -4,9 +4,5 @@ export const selectUserById = async (id: number): Promise<any> => {
   const result = await connection("ToDoListUser")
     .select("*")
     .where("id", id)
-  
-  // .raw(`
-  //   SELECT * FROM User WHERE id = "${id}"
-  // `)
   return result[0]
 }
