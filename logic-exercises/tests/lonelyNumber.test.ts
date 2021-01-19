b20fe5131b814a3bfe2433548a69f680109d28b6
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2021-01-19 18:39:42 -0300

    Adicionando exercício e testes para os casos propostos.

--- logic-exercises/tests/lonelyNumber.test.ts
+++ logic-exercises/tests/lonelyNumber.test.ts
@@ -1,17 +1,17 @@
 import { lonelyNumber } from "../src/lonelyNumber"
 
-describe("Indentify the number that is not paired in an array", () => {
+describe.skip("Indentify the number that is not paired in an array", () => {
 
   test("Case 1: [2,2,1] - 1", () => {
     const result = lonelyNumber([2,2,1]);
 
     expect(result).toBe(1);
-  })
+  });
 
   test("Case 2: [4,1,2,1,2] - 4", () => {
     const result = lonelyNumber([4,1,2,1,2]);
 
     expect(result).toBe(4);
-  })
+  });
 
-})
+});
